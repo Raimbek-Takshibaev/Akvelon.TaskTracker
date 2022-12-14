27ea9acf5f81e96378197f1b62d10485986440e7
Raimbek-Takshibaev <raishi24e@gmail.com>
2022-12-14 20:58:41 +0600

    Implemented cornerstone of entire project by adding api, data and presentation layers into project

---
+++ Akvelon.TaskTracker.Data/Helpers/TaskTrackerDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Akvelon.TaskTracker.Data.Helpers;
+
+public partial class TaskTrackerDbContext : DbContext
+{
+    public TaskTrackerDbContext()
+    {
+    }
+
+    public TaskTrackerDbContext(DbContextOptions<TaskTrackerDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;port=5432;Username=postgres;Password=qqqqqqqq1;Database=task_tracker_db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
